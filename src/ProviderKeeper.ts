b751a97ab64d0a2432f979156745f23c9993ea0e
smelnikov <smelnikov@web3tech.ru>
2021-09-02 15:50:52 +0300

    Provider skeleton

--- src/ProviderKeeper.ts
+++ src/ProviderKeeper.ts
@@ -1,37 +1,61 @@
-import {IConnectOptions, IProvider, ITypedData, IUserData, TLong, TTransactionParamWithType} from '@waves/signer';
-import {IWithId, TTransactionWithProofs} from '@waves/ts-types';
+import {AuthEvents, ConnectOptions, Handler, Provider, TypedData, UserData, SignerTx, SignedTx} from '@waves/signer';
+import { EventEmitter } from 'typed-ts-events';
 import TWavesKeeperApi = WavesKeeper.TWavesKeeperApi;
 import IAuthData = WavesKeeper.IAuthData;
 
-export class ProviderKeeper implements IProvider {
-    public user: IUserData | null = null;
+
+export class ProviderKeeper implements Provider {
+
+    public user: UserData | null = null;
     private readonly _api: TWavesKeeperApi = window.WavesKeeper;
     private readonly _authData: IAuthData;
-    private _options: IConnectOptions = {
+    private _options: ConnectOptions = {
         NETWORK_BYTE: 'W'.charCodeAt(0),
         NODE_URL: 'https://nodes.wavesnodes.com',
     };
+    private readonly emitter: EventEmitter<AuthEvents> = new EventEmitter<AuthEvents>();
 
     constructor(authData: IAuthData) {
         this._authData = authData;
         // TODO wait WavesKeeper extension API
     }
 
-    public connect(options: IConnectOptions): Promise<void> {
+    public on<EVENT extends keyof AuthEvents>(
+        event: EVENT,
+        handler: Handler<AuthEvents[EVENT]>
+    ): Provider {
+        this.emitter.on(event, handler);
+
+        return this;
+    }
+
+    public once<EVENT extends keyof AuthEvents>(
+        event: EVENT,
+        handler: Handler<AuthEvents[EVENT]>
+    ): Provider {
+        this.emitter.once(event, handler);
+
+        return this;
+    }
+
+    public off<EVENT extends keyof AuthEvents>(
+        event: EVENT,
+        handler: Handler<AuthEvents[EVENT]>
+    ): Provider {
+        this.emitter.once(event, handler);
+
+        return this;
+    }
+
+    public connect(options: ConnectOptions): Promise<void> {
         this._options = options;
         // TODO check networkCode, networkServer the same as WavesKeeper.publicState
         return Promise.resolve()
     }
 
-    public sign(
-        list: Array<TTransactionParamWithType>
-    ): Promise<Array<TTransactionWithProofs<TLong> & IWithId>> {
-        return Promise.resolve() as any;
-    }
-
-    public login(): Promise<IUserData> {
-        // TODO
-        return Promise.resolve() as any
+    public login(): Promise<UserData> {
+        return this._api.auth(this._authData)
+            .then(userData => this.user = userData);
     }
 
     public logout(): Promise<void> {
@@ -39,12 +63,17 @@ export class ProviderKeeper implements IProvider {
         return Promise.resolve();
     }
 
-    public signTypedData(data: Array<ITypedData>): Promise<string> {
+    public signMessage(data: string | number): Promise<string> {
+        // TODO
+        return Promise.resolve() as any
+    }
+
+    public signTypedData(data: Array<TypedData>): Promise<string> {
         // TODO
         return Promise.resolve() as any;
     }
 
-    public signMessage(data: string | number): Promise<string> {
+    public sign<T extends Array<SignerTx>>(toSign: T): Promise<SignedTx<T>> {
         // TODO
         return Promise.resolve() as any;
     }
