0365da2e7a3add0f060a1b0cf181e2f5f73b7df9
smelnikov <smelnikov@web3tech.ru>
2021-09-03 19:05:13 +0300

    Throw exception on signer and keeper network mismatch.

--- src/ProviderKeeper.ts
+++ src/ProviderKeeper.ts
@@ -1,10 +1,19 @@
-import {AuthEvents, ConnectOptions, Handler, Provider, SignedTx, SignerTx, TypedData, UserData} from '@waves/signer';
+import {
+    AuthEvents,
+    ConnectOptions,
+    Handler,
+    Provider,
+    SignedTx,
+    SignerTx,
+    TypedData,
+    UserData,
+} from '@waves/signer';
 import {EventEmitter} from 'typed-ts-events';
 import {keeperTxFactory} from "./adapter";
 import TWavesKeeperApi = WavesKeeper.TWavesKeeperApi;
 import IAuthData = WavesKeeper.IAuthData;
 import TTypedData = WavesKeeper.TTypedData;
-
+import TSignTransactionPackageData = WavesKeeper.TSignTransactionPackageData;
 
 export class ProviderKeeper implements Provider {
 
@@ -19,7 +28,6 @@ export class ProviderKeeper implements Provider {
 
     constructor(authData: IAuthData) {
         this._authData = authData;
-        // TODO wait WavesKeeper extension API
     }
 
     public on<EVENT extends keyof AuthEvents>(
@@ -51,8 +59,16 @@ export class ProviderKeeper implements Provider {
 
     public connect(options: ConnectOptions): Promise<void> {
         this._options = options;
-        // TODO check networkCode, networkServer the same as WavesKeeper.publicState
-        return Promise.resolve()
+        return this._api.publicState().then(state => {
+            let {code: keeperCode, server: keeperURL} = state.network;
+            let {NETWORK_BYTE: signerByte, NODE_URL: signerURL} = this._options;
+
+            if (keeperCode.charCodeAt(0) !== signerByte) {
+                throw new Error(`Network byte mismatch: Signer(${signerByte}), Keeper(${keeperCode.charCodeAt(0)})`);
+            } else if (keeperURL.replace(/\/$/, "") !== signerURL.replace(/\/$/, "")) {
+                throw new Error(`Node URL mismatch: Signer(${signerURL}) Keeper(${keeperURL})`);
+            }
+        })
     }
 
     public login(): Promise<UserData> {
@@ -62,7 +78,6 @@ export class ProviderKeeper implements Provider {
 
     public logout(): Promise<void> {
         this.user = null;
-        // TODO connect to extension via Port and close his connection
         return Promise.resolve();
     }
 
@@ -90,11 +105,11 @@ export class ProviderKeeper implements Provider {
 
     public sign<T extends Array<SignerTx>>(toSign: T): Promise<SignedTx<T>> {
         return this._api.signTransactionPackage(
-            toSign.map(tx => keeperTxFactory(tx)) as any
+            toSign.map(tx => keeperTxFactory(tx)) as TSignTransactionPackageData
         ).then(data => {
             return Promise.resolve(
-                data.map(tx => JSON.parse(tx)) as any
+                data.map(tx => JSON.parse(tx))
             );
-        });
+        }) as Promise<SignedTx<T>>;
     }
 }
