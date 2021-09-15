084c47b28373f5ed0d0cf038560d47d7020f8280
smelnikov <smelnikov@web3tech.ru>
2021-09-15 21:26:15 +0300

    Fetch extension without filesystem usage.
    Selenium from testcontainers.

--- packages/provider-keeper/tests/ui.spec.ts
+++ packages/provider-keeper/tests/ui.spec.ts
@@ -1,43 +1,53 @@
 import { expect } from 'chai';
 import { Builder, until, By } from 'selenium-webdriver';
 import * as chrome from 'selenium-webdriver/chrome';
-import * as fs from 'fs';
-import * as path from 'path';
-import Extension from './utils/extension';
+import fetchById from './utils/extension';
+import { GenericContainer } from 'testcontainers';
 
-let extension, driver;
+let driver, sc;
+const s = 1000,
+    m = 60000;
 const extensionId = 'lpilbniiabackdjcionkobglmddfbcjo';
 
-before(async () => {
-    extension = new Extension(extensionId);
-    const extensionPath = path.resolve(__dirname, '../extension.crx');
-    await extension.saveTo(extensionPath);
+before(async function () {
+    this.timeout(60 * s);
 
-    let options = new chrome.Options().addExtensions(
-        fs.readFileSync(extensionPath, {
-            encoding: 'base64',
-        })
-    );
-    driver = new Builder().forBrowser('chrome').setChromeOptions(options).build();
+    const ext = fetchById(extensionId);
+    // selenium container
+    sc = await new GenericContainer('selenium/standalone-chrome').withExposedPorts(4444).start();
+    // selenium webdriver
+    driver = new Builder()
+        .forBrowser('chrome')
+        .usingServer(`http://${sc.getHost()}:${sc.getMappedPort(4444)}/wd/hub`)
+        .setChromeOptions(new chrome.Options().addExtensions(await ext))
+        .build();
 });
 
-after(async () => {
-    if (driver) await driver.quit();
-    if (extension) extension.delete();
+after(async function () {
+    this.timeout(60 * s);
+
+    driver && (await driver.quit());
+    sc && (await sc.stop());
 });
 
 describe('Selenium webdriver', function () {
-    // @ts-ignore
-    this.timeout(0);
+    this.timeout(10 * m);
+    // first test may have extra lag time
+    const lag = 30 * s;
 
     it('extension is loaded', async () => {
         driver.get(`chrome-extension://${extensionId}/popup.html`);
         let elem = await driver.wait(
             until.elementLocated(By.css('.app button[type=submit]')),
-            5000,
-            'Timeout after 10 sec',
+            lag + 10 * s,
+            'Timeout',
             100
         );
+        expect(await elem.getText()).to.be.equal('Get Started');
+    });
+
+    it('second test should be faster', async () => {
+        let elem = await driver.wait(until.elementLocated(By.css('.app button[type=submit]')), 10 * s, 'Timeout', 100);
         expect(await elem.getText()).to.be.equal('Get Started');
     });
 });
--- packages/provider-keeper/tests/utils/extension.ts
+++ packages/provider-keeper/tests/utils/extension.ts
@@ -1,53 +1,40 @@
-import { createWriteStream, unlinkSync } from 'fs';
 import { get } from 'https';
 
-export default class Extension {
-    id: string;
-    path?: string;
+export default async function fetchById(id: string): Promise<string> {
+    const url =
+        'https://clients2.google.com/service/update2/crx?' +
+        'response=redirect' +
+        '&os=mac' +
+        '&arch=x86-64' +
+        '&os_arch=x86-64' +
+        '&nacl_arch=x86-64' +
+        '&prod=chromecrx' +
+        '&prodchannel=unknown' +
+        '&prodversion=93.0.4577.63' +
+        '&acceptformat=crx2,crx3' +
+        '&x=id%3D' +
+        id +
+        '%26uc';
 
-    constructor(id: string) {
-        this.id = id;
-    }
+    return new Promise<string>((resolve, reject) => {
+        let body = '';
 
-    getUrl = (): string => {
-        return (
-            'https://clients2.google.com/service/update2/crx?' +
-            'response=redirect' +
-            '&os=mac' +
-            '&arch=x86-64' +
-            '&os_arch=x86-64' +
-            '&nacl_arch=x86-64' +
-            '&prod=chromecrx' +
-            '&prodchannel=unknown' +
-            '&prodversion=93.0.4577.63' +
-            '&acceptformat=crx2,crx3' +
-            '&x=id%3D' +
-            this.id +
-            '%26uc'
-        );
-    };
-
-    saveTo = async (path) => {
-        let cls = this;
-
-        return new Promise<void>((resolve) => {
-            get(this.getUrl(), (response) => {
-                if (response.statusCode === 302) {
-                    get(response.headers.location!, (response) => {
-                        let file = createWriteStream(path);
-                        response.pipe(file);
-                        cls.path = path;
-                        file.on('finish', resolve);
-                    });
-                }
-            });
+        get(url, (response) => {
+            if (response.statusCode === 302) {
+                get(response.headers.location!, (response) => {
+                    response.setEncoding('base64');
+                    response
+                        .on('data', (chunk) => {
+                            body += chunk;
+                        })
+                        .on('end', () => {
+                            resolve(body);
+                        })
+                        .on('error', (err) => {
+                            reject(err);
+                        });
+                });
+            }
         });
-    };
-
-    destroy = () => {
-        if (this.path) {
-            unlinkSync(this.path);
-            delete this.path;
-        }
-    };
+    });
 }
