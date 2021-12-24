cfd98ab8ec5bfacf0c88ef58fbd5e79a27b13d21
smelnikov <smelnikov@users.noreply.github.com>
2021-12-24 17:34:31 +0300

    KEEP-158 Make sure Waves Keeper is on the same network as Signer (#22)

---
+++ provider/src/decorators.ts
@@ -0,0 +1,29 @@
+import { ProviderKeeper } from './ProviderKeeper';
+import { ConnectOptions } from '@waves/signer';
+
+export function ensureNetwork(target, propertyKey: string, descriptor: PropertyDescriptor) {
+    const origin = descriptor.value;
+
+    descriptor.value = function (
+        this: { [Key in keyof ProviderKeeper]: ProviderKeeper[Key] } & {
+            _api: WavesKeeper.TWavesKeeperApi;
+            _options: ConnectOptions;
+        },
+        ...args: Array<any>
+    ) {
+        const api = this._api;
+        return api.publicState().then(state => {
+            const nodeUrl = state.network.server;
+            const networkByte = state.network.code.charCodeAt(0);
+            if (
+                nodeUrl.replace(/\/$/, '') !== this._options.NODE_URL.replace(/\/$/, '') ||
+                networkByte !== this._options.NETWORK_BYTE
+            ) {
+                throw new Error(
+                    `Invalid connect options. Signer connect (${this._options.NODE_URL} ${this._options.NETWORK_BYTE}) not equals keeper connect (${nodeUrl} ${networkByte})`
+                );
+            }
+            return origin.apply(this, args);
+        });
+    };
+}
