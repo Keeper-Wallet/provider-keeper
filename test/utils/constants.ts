123169b613f28880c3473b107a9dd883f829450d
smelnikov <smelnikov@uniqsoft.ae>
2023-02-09 21:34:51 +0400

    chore: remove e2e tests

--- test/utils/constants.ts
+++
@@ -1,6 +0,0 @@
-export const DEFAULT_PASSWORD = 'default-password';
-export const DEFAULT_MINER_SEED = 'waves private node seed with waves tokens';
-export const ISSUER_SEED =
-  'issuer account that will receive tokens from generator';
-export const USER_1_SEED = 'user number one that used in test scenarios';
-export const USER_2_SEED = 'user number two that used in test scenarios';
